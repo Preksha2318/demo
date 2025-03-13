a194ab12ca9ec9dca2506685b01824cc9ffa0ab5
Preksha K <preksha23k@gmail.com>
2025-03-13 15:34:44 +0530

    project 8

---
+++ demo/src/main/java/com/Preksha2318/App.java
@@ -0,0 +1,13 @@
+package com.Preksha2318;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
